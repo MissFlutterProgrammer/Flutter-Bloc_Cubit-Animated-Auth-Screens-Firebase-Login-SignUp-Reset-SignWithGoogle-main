4cbfe17581f154c95131c60b85dadacd04895c6d
rezaiemehrnegar <rezaiemehrnegar@gmail.com>
2025-03-16 22:04:28 +0330

    3/16/2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 import 'routing/app_router.dart';
 import 'routing/routes.dart';
 import 'theming/colors.dart';
@@ -13,9 +13,9 @@ late String initialRoute;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Future.wait([
-    // Firebase.initializeApp(
-    //   options: DefaultFirebaseOptions.currentPlatform,
-    // ),
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    ),
     ScreenUtil.ensureScreenSize(),
     preloadSVGs(['assets/svgs/google_logo.svg'])
   ]);
